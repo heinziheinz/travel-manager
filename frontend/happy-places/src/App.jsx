155a9276ffae1e203eb5a08e9912368897fcbe61
johnnyRobot <beitschalbua@gmx.at>
2023-05-16 09:32:33 +0200

    saved

--- frontend/happy-places/src/App.jsx
+++ frontend/happy-places/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
-
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+// import { Switch } from "react-router-dom";
+// https://stackoverflow.com/questions/69843615/switch-is-not-exported-from-react-router-dom
+// https://reactrouter.com/en/6.9.0/upgrading/v5#upgrade-all-switch-elements-to-routes
 import './App.css'
 
 function App() {
